3b90b4f4f58da521459d3827fd710252b8c42232
freezeyang <freezeyang@gmail.com>
2012-10-02 15:41:39 -0400

    Backup Login Page of AWOL

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LoginTest::Application.config.secret_token = '52b766655d128a5b29a02fc538123595f3e459dd73ac15a1ec39c0737aa4f2301fec81db4d61f0250ac79ef24b7876fa388b70ba7229e9ffac52792926959663'
